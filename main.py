1d65ddac27195ade7ba27dc5b09d32c7e9d4e43f
plaxxo <oli.hahn3@gmail.com>
2020-11-03 00:44:08 +0100

    FSRBot 1.0

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f'Pong --> {round(client.latency * 1000)} ms')
-    await ctx.send(ctx.channel.id)
 
 
 @client.command()
