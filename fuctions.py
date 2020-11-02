f0d56519750fc27d202b31e2d90e55a568b4f098
plaxxo <oli.hahn3@gmail.com>
2020-11-02 04:34:08 +0100

    fworking version for fsr server

--- fuctions.py
+++ fuctions.py
@@ -5,10 +5,10 @@ from pw import channelID
 # Test ob Rolle schon vergeben ist, dass man keine zwei Kurse bekommen kann
 
 def rolecheck(user):
-    role1 = discord.utils.get(user.guild.roles, name='winf')
-    role2 = discord.utils.get(user.guild.roles, name='wipäd')
-    role3 = discord.utils.get(user.guild.roles, name='wing')
-    role4 = discord.utils.get(user.guild.roles, name='wiwi')
+    role1 = discord.utils.get(user.guild.roles, name='WInformatiker')
+    role2 = discord.utils.get(user.guild.roles, name='WIngeneur')
+    role3 = discord.utils.get(user.guild.roles, name='WPädagoge')
+    role4 = discord.utils.get(user.guild.roles, name='WWissenschaftler')
 
     if role1 in user.roles:
         return False
